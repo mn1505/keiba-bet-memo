ab2f900747c88e1eb950b36232f39108e4f13783
Michiya Nagahisa <dkihg608@yahoo.co.jp>
2026-05-01 00:08:35 +0900

    Create keiba bet memo app

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "kaime-memo-v1";
+
+const CACHE_FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon.svg"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(CACHE_FILES);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+
+          return null;
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request);
+    })
+  );
+});
